0f21606edcfc594335dfeb89b9086deb5d5a58c2
Jim Holmström <jim.holmstroem@gmail.com>
2013-07-19 10:35:26 +0200

    think I solved the bin/oct/dec/hex but I need it too compile

--- simple_parser.hs
+++ simple_parser.hs
@@ -1,6 +1,6 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
-
+import Numeric
 import Control.Monad
 
 data LispVal = Atom String
@@ -48,10 +48,12 @@ parseString = do
         where parseEscapeSeq = char '\\' >> (return . escapeChar =<< oneOf escapeableChar)
               parseNormalCharacter = noneOf "\"\\"
 
-data BaseNumber = BinaryNumber String
-                | OctalNumber String
-                | DecimalNumber String
-                | HexadeciamlNumber String
+readBin = readBin_reverse . reverse
+    where readBin_reverse ('0':xs) = 0 + 2 * readBin_reverse xs
+          readBin_reverse ('1':xs) = 1 + 2 * readBin_reverse xs
+          readBin_reverse [] = 0
+
+(readOct, readDec, ReadHex) = map (fst . (!!0) $) [Numeric.readOct, Numeric.ReadDec, Numeric.ReadHex]
 
 parseNumber :: Parser LispVal
 --parseNumber = (liftM (Number . read)) $ many1 digit
@@ -59,13 +61,21 @@ parseNumber :: Parser LispVal
 --    x <- many1 digit
 --    return $ Number . read $ x
 --parseNumber = many1 digit >>= return . Number . read
-parseNumber = return . Number . read =<< (normalForm <|> (char '#' >> oneOf "bodx" >> (many1 digit))) -- return the right form in the last expression :)
-    where normalForm = many1 digit
-          readBase = BinaryNumber c = readBin c --pattern match the types
-                   | OctalNumber c = readOct c
-                   | DecimalNumber c = read c
-                   | HexadecimalNumber c = readHex c
-
+parseNumber = return . Number =<<
+    (
+        return . readDec =<< many1 digit <|>
+        (
+            char '#' >>
+                (return . readBin =<< (char "b" >> (many1 binaryDigit))) <|>
+                (return . readOct =<< (char "o" >> (many1 octalDigit))) <|>
+                (return . readDec =<< (char "d" >> (many1 decimalDigit))) <|>
+                (return . readHex =<< (char "x" >> (many1 hexadecimalDigit)))
+        )
+    )
+    where binaryDigit = "01"
+          octalDigit = "01234567"
+          decimalDigit = "01234567" --TODO make this a generalFunction tokensUpTo "a", remember to use lowercase
+          hexadecimalDigit = "0123456789abcdef"
 
 parseAtom :: Parser LispVal
 parseAtom = do
